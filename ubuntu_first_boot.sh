89ae3aefb91799295aa8579461fddf36b7b18878
nanite10 <44302569+nanite10@users.noreply.github.com>
2020-06-06 22:26:10 -0400

    Update ubuntu_first_boot.sh

--- ubuntu_first_boot.sh
+++ ubuntu_first_boot.sh
@@ -1,3 +1,3 @@
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install vim rsync tmux wget iptraf-ng iperf3 cifs-utils
+sudo apt install vim rsync tmux wget iptraf-ng iperf3 cifs-utils git glusterfs-client linux-headers-$(uname -r) linux-image-$(uname -r) libncurses-dev flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf -y
