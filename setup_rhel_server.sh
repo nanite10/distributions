a693a3c0c835421692ddc87a4f60503ebc05a49c
Andrew Onan <nanite10@gmail.com>
2021-04-11 17:57:00 -0400

    distributions: Updated setup_rhel_server.sh

--- setup_rhel_server.sh
+++ setup_rhel_server.sh
@@ -3,4 +3,4 @@ subscription-manager register --auto-attach
 subscription-manager repos --enable "rhel-*-optional-rpms" --enable "rhel-*-extras-rpms"  --enable "rhel-ha-for-rhel-*-server-rpms"
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum update -y
-yum install -y git vim ansible
+yum install -y git vim ansible tmux wget net-tools
