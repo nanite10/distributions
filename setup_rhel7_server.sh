6363dcbf56cd2836e32e2fb712d7a61d99ee239d
nanite10 <nanite10@gmail.com>
2021-04-19 16:49:45 -0400

    distributions: Added sudo and /etc/issue to rhel setup scripts.

--- setup_rhel7_server.sh
+++ setup_rhel7_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-subscription-manager register --auto-attach
-subscription-manager repos --enable "rhel-*-optional-rpms" --enable "rhel-*-extras-rpms"  --enable "rhel-ha-for-rhel-*-server-rpms"
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum update -y
-yum install -y git vim ansible tmux wget net-tools cifs-utils nfs-utils bind-utils dkms kernel-devel kernel-headers gdisk
+sudo subscription-manager register --auto-attach
+sudo subscription-manager repos --enable "rhel-*-optional-rpms" --enable "rhel-*-extras-rpms"  --enable "rhel-ha-for-rhel-*-server-rpms"
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum update -y
+sudo yum install -y git vim ansible tmux wget net-tools cifs-utils nfs-utils bind-utils dkms kernel-devel kernel-headers gdisk
+sudo cp -p /etc/issue /etc/issue.`date +%s`
+sudo cp -f etc/issue /etc/issue
+sudo chown root: /etc/issue
+sudo chmod 0644 /etc/issue
--- setup_rhel8_server.sh
+++ setup_rhel8_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-subscription-manager register --auto-attach
-subscription-manager repos --enable "codeready-builder-for-rhel-8-x86_64-rpms"
-dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-dnf update -y
-dnf install -y git vim ansible tmux wget net-tools cifs-utils nfs-utils bind-utils dkms kernel-devel kernel-headers gdisk
+sudo subscription-manager register --auto-attach
+sudo subscription-manager repos --enable "codeready-builder-for-rhel-8-x86_64-rpms"
+sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf update -y
+sudo dnf install -y git vim ansible tmux wget net-tools cifs-utils nfs-utils bind-utils dkms kernel-devel kernel-headers gdisk
+sudo cp -p /etc/issue /etc/issue.`date +%s`
+sudo cp -f etc/issue /etc/issue
+sudo chown root: /etc/issue
+sudo chmod 0644 /etc/issue
